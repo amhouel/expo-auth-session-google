e5a5fc2647a8c8270fc60ba6799c3bf9b8e7c2d8
Alice Houel <alice@reach.vote>
2020-11-30 19:57:09 -0500

    App set up for Google expo-auth-session switch account bug demo

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { View, Text, Button } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId:
+      "Your_Google_Web_Client_ID.apps.googleusercontent.com",
+    selectAccount: true,
+  });
+
+  const [userEmail, setUserEmail] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const getUserEmail = async (token) => {
+    const user = await fetch(
+      "https://openidconnect.googleapis.com/v1/userinfo",
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const userData = await user.json();
+    setUserEmail(userData.email);
+    setIsLoading(false);
+  };
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { authentication } = response;
+      getUserEmail(authentication.accessToken);
+    }
+  }, [response, isLoading]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{alignItems:"center", paddingTop: 100}}>
+      <Button
+        disabled={!request}
+        title="Select Account"
+        onPress={() => {
+          promptAsync().then(() => setIsLoading(true));
+        }}
+      />
+      {isLoading ? (
+        <Text style={{marginTop: 20}}>Loading...</Text>
+      ) : (
+        userEmail && <Text style={{marginTop: 20}}>Account selected: {userEmail}</Text>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
